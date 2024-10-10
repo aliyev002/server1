faf01528ee2c02b25946d816398da82cbd350152
Aliyev Togrul Ceyhun <Aliye_du26@itstep.edu.az>
2024-10-10 17:51:05 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Net.Sockets;
+using System.Net;
+
+var ip = IPAddress.Parse("10.1.18.7");
+var port = 27001;
+var ep = new IPEndPoint(ip, port);
+var listener = new TcpListener(ep);
+
+listener.Start();
+while (true)
+{
+    var client = listener.AcceptTcpClient();
+    _ = Task.Run(() =>
+    {
+        var networkStream = client.GetStream();
+        var remoteEp=client.Client.RemoteEndPoint as IPEndPoint;
+        var directoryPath=Path.Combine(Environment.CurrentDirectory,remoteEp!.Address.ToString());
+
+        if (Directory.Exists(directoryPath))
+            Directory.CreateDirectory(directoryPath);
+
+        var path = Path.Combine(directoryPath,$"{DateTime.Now:dd.MM.yyyy.HH.mm.ss}.png");
+
+        using (var fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write))
+        {
+            int len = 0;
+            var bytes = new byte[len];
+            while ((len = networkStream.Read(bytes, 0, len)) > 0)
+            {
+                fs.Write(bytes, 0, len);
+            }
+
+        };
+        Console.WriteLine("Files Received");
+        client.Close();
+
+    });
+}
+
+
